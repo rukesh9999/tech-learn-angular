602a4fda588e67d361a86a34128822de0cd4c949
ganesh <ganesh2223@gmail.com>
2023-10-29 12:55:26 +0530

    added technology.ts file

---
+++ tech-learn-angular/tech-learn/src/app/models/StatusMain.ts
@@ -0,0 +1,9 @@
+export class StatusMain{
+    
+     statusMainId:number;
+	 name:number;
+     description:number;
+	 createdDate:number;
+	 modifiedDate:number;
+	
+}
---
+++ tech-learn-angular/tech-learn/src/app/models/Technology.ts
@@ -0,0 +1,14 @@
+export class Technology{
+   technologyId:number
+   code:string
+   name:string
+   description:string
+   createdDate:Date
+   modifiedDate:Date
+   expectedCompletionDate:Date
+   totalTimeToComplete:string
+   statusName:string
+   createdBy:string
+
+
+}
